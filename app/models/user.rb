c4b7809dfe8d78e827421a8da72db1ed22d6bfe2
Uuya <a1b23cd@icloud.com>
2019-01-20 12:34:33 +0900

    Add validates length maximum 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
           authentication_keys: [:name]
 
-  validates_uniqueness_of :name
-  validates_presence_of   :name
+  validates :name, presence: true, uniqueness: true, length: { maximum: 8 }
 
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
