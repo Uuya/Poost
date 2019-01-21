0895a66a7dc686dd1b4ca2f40ef1309649e6592d
Uuya <a1b23cd@icloud.com>
2019-01-21 20:45:13 +0900

    Add before_action :authenticate_user! & Add index fav ranking

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,9 @@
 class FavoritesController < ApplicationController
+before_action :authenticate_user!
+layout 'index'
 
 def index
+ @favs = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(5).pluck(:post_id))
 end
 
 end
